1992996425878c5ee31ee585f6bc1f8ab9df7748
jeffer739 <64267672+jeffer739@users.noreply.github.com>
2026-06-07 00:04:47 +0000

    Deploying to gh-pages from @ jeffer739/nerdweb007@70fbf70a84d625bd80790b0453d824a972fbb47d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Chuks Clinton — Penetration Tester | Web Application Security",
           section: "Navigation",
           handler: () => {
             window.location.href = "/nerdweb007/cv/";
